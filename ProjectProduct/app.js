7d48af55c24ab45b40176dd477bb3f78d5622810
wmccrthy <115899870+wmccrthy@users.noreply.github.com>
2023-05-05 13:27:23 -0400

    Add files via upload

--- ProjectProduct/app.js
+++ ProjectProduct/app.js
@@ -1,8 +1,16 @@
+var select = document.querySelector(".selector");
 var courses = document.querySelector(".courses");
 var selector = document.querySelector("select");
 selector.onchange = function () {
-    courses.innerHTML = "<a href='demo.html' class='nav-item'>Demo Course 111</a>"
+    // courses.innerHTML = "<a href='demo.html' class='nav-item'>Demo Course 111</a>"
+    courses.style.opacity = "1";
 }
+
+var searchProf = document.querySelector("#search-prof");
+searchProf.addEventListener("click", function () {
+    select.innerHTML = "";
+    // select.innerHTML = '<select name="" id="department"><option selected disabled>Select a Professor</option><option value="DemoDepartment">John Doe</option></select>';
+}) 
 
 
 
